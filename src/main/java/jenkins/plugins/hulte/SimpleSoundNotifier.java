557ddbab0a247487124e4c588d20d3d397255f72
Lars Hvile <lars@hulte.net>
2011-08-30 23:24:04 +0200

    completed all the proof-of-concept stuff, time to clean it up...

--- src/main/java/jenkins/plugins/hulte/SimpleSoundNotifier.java
+++ src/main/java/jenkins/plugins/hulte/SimpleSoundNotifier.java
@@ -1,23 +1,41 @@
 package jenkins.plugins.hulte;
 
+import jenkins.model.Jenkins;
+
+import org.kohsuke.stapler.bind.JavaScriptMethod;
+
 import hudson.Extension;
 import hudson.Plugin;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.model.listeners.RunListener;
 
-// stapler json/xml @ExportedBean
+/**
+ * 
+ * 
+ * @author Lars Hvile
+ */
 public final class SimpleSoundNotifier extends Plugin {
     
     {
         System.out.println("> SimpleSoundNotifier started.. ");
     }
     
+    
+    public static SimpleSoundNotifier instance() {
+        System.out.println("yep");
+        // Jenkins.getInstance().getPlugin(shortName)
+        // Jenkins.getInstance().getPluginManager().getPlugin(shortName)
+        return Jenkins.getInstance().getPlugin(SimpleSoundNotifier.class);
+    }
+    
+    
     @Extension
-    public static final class GlobalBuildListener extends RunListener<Run<?, ?>> {
+    public static final class GlobalBuildListener extends RunListener<Run<?, ?>> { // TODO dedicated class? has to report build-results into some shared structure..
         
         {
             System.out.println("> GlobalBuildListener started.. ");
+            // hudson.model.Hudson.getInstance().getPl
         }
         
         @Override
@@ -26,5 +44,11 @@ public final class SimpleSoundNotifier extends Plugin {
             // TODO do something useful...
             System.out.println("> " + r.getDisplayName() + " / " + r.getResult());
         }
+    }
+    
+    
+    @JavaScriptMethod
+    public String wazzup() {
+        return "http://localhost:8080/plugin/simple-sound-notifier/test.wav"; // TODO relative / not hardcoded plz
     }
 }
