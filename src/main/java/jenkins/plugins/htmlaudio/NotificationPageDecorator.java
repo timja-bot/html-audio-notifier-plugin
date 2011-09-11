b7896aea787999c400e1e341f6974fd4e2734d9c
Lars Hvile <lars@hulte.net>
2011-09-11 21:51:09 +0200

    renamed & commented the page-decorator

--- src/main/java/jenkins/plugins/htmlaudio/NotificationPageDecorator.java
+++
@@ -1,9 +0,0 @@
-package jenkins.plugins.htmlaudio;
-
-import hudson.Extension;
-import hudson.model.PageDecorator;
-
-
-@Extension
-public class NotificationPageDecorator extends PageDecorator {
-}
---
+++ src/main/java/jenkins/plugins/htmlaudio/PageDecorator.java
@@ -0,0 +1,13 @@
+package jenkins.plugins.htmlaudio;
+
+import hudson.Extension;
+
+
+/**
+ * Adds the required javascript/html for the html-audio stuff through jelly-pages.
+ * 
+ * @author Lars Hvile
+ */
+@Extension
+public class PageDecorator extends hudson.model.PageDecorator {
+}
