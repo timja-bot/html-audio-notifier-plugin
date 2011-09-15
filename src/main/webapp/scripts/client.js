bcd786c9a2ef5d9ca9c6f33a339f2cb0f87579fa
Lars Hvile <lars@hulte.net>
2011-09-15 23:07:36 +0200

    tabs2spaces for the javascript files

--- src/main/webapp/scripts/client.js
+++ src/main/webapp/scripts/client.js
@@ -3,110 +3,110 @@
  */
 var HtmlAudioNotifierClient = Class.create();
 HtmlAudioNotifierClient.prototype = {
-	
-	initialize: function(url, uiElement) {
-		this.url = url;
-		this.uiElement = uiElement;
-		this.player = new AudioPlayer();
-		this.executor = null;
-		
-		var enabled = this.isEnabled();
-		
-		if (enabled == null) {
-			var that = this;
-			
-			new Ajax.Request(url + "/isEnabledByDefault", {
+
+    initialize: function(url, uiElement) {
+        this.url = url;
+        this.uiElement = uiElement;
+        this.player = new AudioPlayer();
+        this.executor = null;
+
+        var enabled = this.isEnabled();
+
+        if (enabled == null) {
+            var that = this;
+
+            new Ajax.Request(url + "/isEnabledByDefault", {
                 method: 'post',
                 onSuccess: function(t) {
-                	var enabled = t.responseText.evalJSON(true).enabled;
-                	that.enable(enabled);
+                    var enabled = t.responseText.evalJSON(true).enabled;
+                    that.enable(enabled);
                 },
                 onFailure: function() {
-                	that.enable(false);
+                    that.enable(false);
                 }
             });
-		} else {
-			this.enable(enabled);
-		}
-	},
-	
-	isEnabled: function() {
-		var val = readCookie("htmlAudioNotificationsEnabled");
-		return val == null
-			? null
-			: val == 'true';
-	},
-	
-	toggle: function() {
-		this.enable(this.executor == null);
-	},
+        } else {
+            this.enable(enabled);
+        }
+    },
 
-	enable: function(enabled) {
-		
-		if (enabled) {
-			this.start();
-		} else {
-			this.stop();
-		}
-		
-		this.showEnabledState(enabled)
-		this.storeEnabledState(enabled);
-	},
+    isEnabled: function() {
+        var val = readCookie("htmlAudioNotificationsEnabled");
+        return val == null ? null : val == 'true';
+    },
 
-	start: function() {
-		this.stop();
-		
-		var that = this;
-		this.executor = new PeriodicalExecuter(function() { that.poll(that) }, 5);
-	},
+    toggle: function() {
+        this.enable(this.executor == null);
+    },
 
-	stop: function() {
-		if (this.executor != null) {
-			this.executor.stop();
-		}
-		this.executor = null;
-	},
+    enable: function(enabled) {
 
-	showEnabledState: function(enabled) {
-		this.uiElement.className = enabled
-			? 'enabled'
-			: 'disabled';	
-	},
+        if (enabled) {
+            this.start();
+        } else {
+            this.stop();
+        }
 
-	storeEnabledState: function(enabled) {
-		createCookie("htmlAudioNotificationsEnabled", enabled, 30);
-	},
+        this.showEnabledState(enabled)
+        this.storeEnabledState(enabled);
+    },
 
-	poll: function(client) {
-		
-		new Ajax.Request(client.url + "/next", {
+    start: function() {
+        this.stop();
+
+        var that = this;
+        this.executor = new PeriodicalExecuter(function() {
+            that.poll(that)
+        }, 5);
+    },
+
+    stop: function() {
+        if (this.executor != null) {
+            this.executor.stop();
+        }
+        this.executor = null;
+    },
+
+    showEnabledState: function(enabled) {
+        this.uiElement.className = enabled ? 'enabled' : 'disabled';
+    },
+
+    storeEnabledState: function(enabled) {
+        createCookie("htmlAudioNotificationsEnabled", enabled, 30);
+    },
+
+    poll: function(client) {
+
+        new Ajax.Request(client.url + "/next", {
             method: 'post',
-            parameters: { previous: client.getPreviousNotification() },
-            
+            parameters: {
+                previous: client.getPreviousNotification()
+            },
+
             onSuccess: function(t) {
-            	var result = t.responseText.evalJSON(true);
-            	
-    			if (!result) {
-    				return;
-    			}
-    			
-    			client.setPreviousNotification(result.currentNotification);
-    			result.notifications.each(function(src) {
-    				client.player.enqueue(src);
-    			});
+                var result = t.responseText.evalJSON(true);
+
+                if (!result) {
+                    return;
+                }
+
+                client.setPreviousNotification(result.currentNotification);
+                result.notifications.each(function(src) {
+                    client.player.enqueue(src);
+                });
             }
         });
-	},
-	
-	getPreviousNotification: function() {
-		if (this.prevNotification === undefined) {
-			this.prevNotification = readCookie("prevHtmlAudioNotification");
-		}
-		return this.prevNotification;
-	},
-	
-	setPreviousNotification: function(n) {
-		this.prevNotification = n;
-		createCookie("prevHtmlAudioNotification", n, 1);
-	}
+    },
+
+    getPreviousNotification: function() {
+        if (this.prevNotification === undefined) {
+            this.prevNotification = readCookie("prevHtmlAudioNotification");
+        }
+        return this.prevNotification;
+    },
+
+    setPreviousNotification: function(n) {
+        this.prevNotification = n;
+        createCookie("prevHtmlAudioNotification", n, 1);
+    }
 };
--- src/main/webapp/scripts/player.js
+++ src/main/webapp/scripts/player.js
@@ -4,99 +4,99 @@
  */
 var AudioPlayer = Class.create();
 AudioPlayer.prototype = {
-	
-	initialize: function() {
-		this.queue = Array();
-		this.playing = false;
-		this.player = document.createElement('audio');
-		
-		var that = this;
-		
-		// start playing only when completely loaded
-		this.player.addEventListener('canplaythrough', function() {
-			that.onLoaded();
-		}, false);
-		
-		// register event-listeners for playing enqueued sounds
-		['ended', 'error'].each(function(e) {
-			that.player.addEventListener(e,
-				function() { audioEndedListener(that, e) },
-				false);
-		});
-	},
-	
-	enqueue: function(source) {
-		log('enqueued ' + source);
-		this.queue.push(source);
-		this.playNext();
-	},
-	
-	playNext: function() {
-		if (this.playing
-				|| this.queue.size() == 0) {
-			return;
-		}
-		
-		this.playing = true;
-		this.registerTimedRestart();
-		
-		var src = this.queue.shift();
-		log('loading ' + src);
-		
-		this.player.setAttribute('src', src);
-	},
-	
-	onLoaded: function() {
-		log('playing ' + this.player.getAttribute('src'));
-		this.clearTimedRestart();
-		this.player.play();
-	},
-	
-	/*
-	 * Restarts playing after x seconds if timer not explicitly cleared, should hopefully catch most
-	 * load-errors..
-	 */
-	registerTimedRestart: function() {
-		this.clearTimedRestart();
-		
-		var that = this;
-		this.timedRestart = setTimeout(function() {
-			log('> timed reset, load failed or timed out');
-			that.playing = false;
-			that.playNext();
-		}, 20000);
-	},
-	
-	clearTimedRestart: function() {
-		if (this.timedRestart) {
-			clearTimeout(this.timedRestart);
-			this.timedRestart = null;
-		}
-	}
-}
 
+    initialize: function() {
+        this.queue = Array();
+        this.playing = false;
+        this.player = document.createElement('audio');
+
+        var that = this;
+
+        // start playing only when completely loaded
+        this.player.addEventListener('canplaythrough', function() {
+            that.onLoaded();
+        }, false);
+
+        // register event-listeners for playing enqueued sounds
+        ['ended', 'error'].each(function(e) {
+            that.player.addEventListener(e, function() {
+                audioEndedListener(that, e)
+            }, false);
+        });
+    },
+
+    enqueue: function(source) {
+        log('enqueued ' + source);
+        this.queue.push(source);
+        this.playNext();
+    },
+
+    playNext: function() {
+        if (this.playing
+                || this.queue.size() == 0) {
+            return;
+        }
+
+        this.playing = true;
+        this.registerTimedRestart();
+
+        var src = this.queue.shift();
+        log('loading ' + src);
+
+        this.player.setAttribute('src', src);
+    },
+
+    onLoaded: function() {
+        log('playing ' + this.player.getAttribute('src'));
+        this.clearTimedRestart();
+        this.player.play();
+    },
+
+    /*
+     * Restarts playing after x seconds if timer not explicitly cleared, should
+     * hopefully catch most load-errors..
+     */
+    registerTimedRestart: function() {
+        this.clearTimedRestart();
+
+        var that = this;
+        this.timedRestart = setTimeout(function() {
+            log('> timed reset, load failed or timed out');
+            that.playing = false;
+            that.playNext();
+        }, 20000);
+    },
+
+    clearTimedRestart: function() {
+        if (this.timedRestart) {
+            clearTimeout(this.timedRestart);
+            this.timedRestart = null;
+        }
+    }
+}
 
 /*
- * Event-listener that records the end-of-playing and starts playing enqueued sounds.
+ * Event-listener that records the end-of-playing and starts playing enqueued
+ * sounds.
  */
 function audioEndedListener(that, reason) {
-	that.playing = false;
-	that.clearTimedRestart();
-	
-	log(reason);
+    that.playing = false;
+    that.clearTimedRestart();
 
-	// play next from queue, but make sure the current event has been fully processed.
-	setTimeout(function() {
-		that.playNext();
-	}, 0);
+    log(reason);
+
+    // play next from queue, but make sure the current event has been fully
+    // processed.
+    setTimeout(function() {
+        that.playNext();
+    }, 0);
 }
-
 
 /*
  * Logs messages to the javascript-console.
  */
-window.console || (console={log:function(){}}) // stub console.log if necessary
+window.console || (console = {log: function() {}}) // stub console.log if necessary
 
 function log(message) {
-	console.log('htmlAudioNotifier: ' + message);
+    console.log('htmlAudioNotifier: ' + message);
 }
